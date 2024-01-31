ed86f2b2ea08a9c1bfd642a1678c5b2f9a8e0e05
chris <taiwoakinfenwa2019@gmail.com>
2024-01-31 07:32:45 +0100

    LandingPage Adjustment

--- src/Components/LandingPageComponents/Tutor.jsx
+++ src/Components/LandingPageComponents/Tutor.jsx
@@ -1,13 +1,29 @@
 import tutor from "../../assets/Image/t.png"
 import EventIcon from "../Icons/EventIcon"
+import EventSlider from "../../assets/Image/bro.png"
+import EventSlider_1 from "../../assets/Image/rafiki.png"
+
+
+// or other themes
+import '@splidejs/react-splide/css';
+
+// or other themes
+import '@splidejs/react-splide/css/skyblue';
+import '@splidejs/react-splide/css/sea-green';
+
+// or only core styles
+import '@splidejs/react-splide/css/core';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+
+
 const Tutor = () => {
   return (
     <>
       <div className="flex flex-col md:flex-row md:px-[72px] px-3">
     <div>
         <h5 className="md:text-[50px] text-[30px] mb-[10px] font-Montserrat font-medium text-[#000] flex text-center">Become a tutor</h5>
-        <div className="flex  flex-col-reverse justify-between gap-8 lg:flex-row">
-        <div className="flex gap-[20px] flex-col">
+        <div className="flex flex-col-reverse justify-between gap-8 lg:flex-row">
+        <div className="flex justify-center gap-[20px] flex-col">
             <p className="md:w-[600px] md:text-[20px] text-[15px] font-light">Join a wide range of tutors from different tech fields, upload courses on aquber.</p>
             <button className="border-2 border-[#198cf6] rounded-md text-[#198cf6] font-inter text-[15px] w-[183px] h-[51px] hover:bg-[#198cf6] hover:text-[#fff]">Become a tutor</button>
         </div>
@@ -15,13 +31,40 @@ const Tutor = () => {
             <img src={tutor} alt="tutorImage" className="object-cover h-[] w-full "/>
         </div>
         </div>
-        <div className="flex justify-between flex-col lg:flex-row mt-[79px] gap-20 md:gap-0 ">
-           <div className="md:px-[225px]">
-            <EventIcon/>
+      
+        <div className="flex  justify-between flex-col lg:flex-row mt-[150px] gap-20 md:gap-0 ">
+          <div className="w-full">
+           <div className=" mt-[0px] w-[350px] -mx-10 md:mx-0 " >
+           <Splide aria-label="My Favorite Images" 
+            options={ {
+             autoplay:true,
+             rewind: true,
+             gap   : '1rem',
+             arrowPath:false,
+             arrows:false
+            } }
+            >
+              <SplideSlide >
+                 <img src={EventSlider} alt="slide" className=""/>
+              </SplideSlide>
+             <SplideSlide>
+             <img src={EventSlider_1} alt="slide" className=""/>
+              </SplideSlide>
+              <SplideSlide>
+                <EventIcon/>
+              </SplideSlide> 
+              <SplideSlide>
+                <EventIcon/>
+              </SplideSlide>
+               <SplideSlide>
+                <EventIcon/>
+              </SplideSlide>
+            </Splide>
            </div>
-           <div className="flex flex-col gap-[26px] lg:-ml-20">
+           </div>
+           <div className="flex flex-col  gap-[26px] lg:-ml-20">
            <h3 className="text-[#000] font-Montserrat md:text-[50px] text-[30px] font-normal flex text-center ">Event and webinar</h3>
-           <p className="md:text-[20px] text-[15px] font-Montserrat font-light text-[#000]">Join techies from different fields, attend and take
+           <p className="md:text-[20px]   text-[15px] font-Montserrat font-light text-[#000]">Join techies from different fields, attend and take
                 part in our events, boot camp and webinar both
                 online and physically.
             </p>
