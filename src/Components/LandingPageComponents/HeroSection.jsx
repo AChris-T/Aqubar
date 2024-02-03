cdbd6316ccdce9ae282570332f8304bbf748c978
chris <taiwoakinfenwa2019@gmail.com>
2024-02-03 14:36:31 +0100

    landing page

--- src/Components/LandingPageComponents/HeroSection.jsx
+++ src/Components/LandingPageComponents/HeroSection.jsx
@@ -11,8 +11,8 @@ import "swiper/css/pagination";
 const HeroSection = () => {
   return (
     <>
-    <div className="h-[450px] mt-[80px] flex justify-between bg-[#101223]">
-      <div className=" px-4 md:px-[50px] lg:px-[70px] flex flex-col justify-center h-full gap-[34px]">
+    <div className="h-[450px] lg:h-[450px] md:h-[330px] mt-[80px] flex justify-between bg-[#101223]">
+      <div className=" px-4 md:px-[50px] lg:px-[70px] flex flex-col justify-center h-full gap-[34px] lg:gap-[34px] md:gap-[10px]">
         <h2 className="text-[#fff] font-jacques text-[60px]">Aquber</h2>
         <p className="font-Montserrat md:w-[350px] lg:w-[620px] flex flex-wrap text-white z-20 font-light text-[20px]">Welcome to our tech page!<br/>
             We are excited to have you here and to equip you with the latest
@@ -26,12 +26,12 @@ const HeroSection = () => {
           autoplay={true}
           spaceBetween={30}
           pagination={true}
-          className="text-white w-full lg:ml-[-130px] md:ml-[-50px] ml-[-160px] gap-72 hidden md:block"
+          className="text-white w-full h-full lg:ml-[-130px] md:ml-[-50px] ml-[-160px] gap-72 hidden md:block"
         >
-             <SwiperSlide><img src={hero3} alt="heroImg" className="w-full  h-full md:object-contain lg:object-cover opacity-70"/></SwiperSlide>
-             <SwiperSlide><img src={hero2} alt="heroImg" className="w-full h-full md:object-contain lg:object-cover opacity-70 "/></SwiperSlide>
-             <SwiperSlide><img src={hero1} alt="heroImg" className="w-full h-full md:object-contain lg:object-cover opacity-70 "/></SwiperSlide>
-             <SwiperSlide><img src={hero4} alt="heroImg" className="w-full h-full md:object-contain lg:object-cover opacity-70 "/></SwiperSlide>
+             <SwiperSlide><img src={hero3} alt="heroImg" className="h-[100%] w-full   opacity-70"/></SwiperSlide>
+             <SwiperSlide><img src={hero2} alt="heroImg" className="h-[100%] w-full opacity-70 "/></SwiperSlide>
+             <SwiperSlide><img src={hero1} alt="heroImg" className="h-[100%] w-full opacity-70 "/></SwiperSlide>
+             <SwiperSlide><img src={hero4} alt="heroImg" className="h-[100%] w-full  opacity-70 "/></SwiperSlide>
         </Swiper>
       </div>
       <div>
