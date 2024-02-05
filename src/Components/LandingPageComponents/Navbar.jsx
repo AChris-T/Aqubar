c16cbde52aeb97758c2a3db3ae80cecb6d4cc909
chris <taiwoakinfenwa2019@gmail.com>
2024-02-05 11:29:19 +0100

    landingPAge

--- src/Components/LandingPageComponents/Navbar.jsx
+++ src/Components/LandingPageComponents/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
       <div className=" flex justify-center items-center font-inter text-[20px] font-normal text-[#353535] md:gap-[31px] gap-2.5 ">
         <IoMenu onClick={open}  className=" text-[50px] md:hidden cursor-pointer"/>
         <a href="/" className="hidden  md:block">Service</a>
-        <a href="/" className="hidden  md:block" >Tech on aqubar</a>
+        <a href="/" className="hidden  md:block" >Teach on aqubar</a>
         <a href="/" className=" hidden  md:flex px-[18px] md:px-[28px]   justify-center items-center text-[15px] font-semibold h-[42px] rounded-[5px] border-2 border-[#198cf6] hover:bg-[#198cf6] hover:text-white">Login</a>
         <a href="/" className="hidden  md:flex w-[90px] md:w-[115px] md:px-[2px]  justify-center items-center text-[15px] font-semibold h-[42px] rounded-[5px] text-[#fff] border-2 bg-[#198cf6] border-[#198cf6] hover:bg-[#4d9de9] hover:border-[#63aaed]">Sign Up</a>
 
