9674a5fbf594a8ef26d28b34915f8fc605f9b72d
chris <taiwoakinfenwa2019@gmail.com>
2024-02-22 16:48:04 +0100

    Effect animations

--- src/Components/LandingPageComponents/Navbar.jsx
+++ src/Components/LandingPageComponents/Navbar.jsx
@@ -7,7 +7,7 @@ import { Drawer  } from '@mantine/core';
 const Navbar = () => {
   const [opened, { open, close }] = useDisclosure(false);
   return (
-    <div className="fixed w-[100%] max-w-[1452px] z-30 ">   
+    <div className="fixed w-[100%] max-w-[1500px] z-30 ">   
     <div className="flex items-center relative  top-0 left-0  bg-white justify-between md:px-[40px]  lg:px-[60px] md:h-[80px] h-[60px] pr-2   z-20">
       <div>
         <img src={logo} alt="logo" width={"147px"} height={"px2rem(52)"} className=""/>
--- src/Components/LandingPageComponents/Services.jsx
+++ src/Components/LandingPageComponents/Services.jsx
@@ -1,8 +1,14 @@
 /* eslint-disable react/no-unescaped-entities */
+import { useEffect } from "react"
 import service from "../../assets/Image/serv.png"
 import service_1 from "../../assets/Image/serve.png"
 import service_2 from "../../assets/Image/served.png"
+import  AOS  from "aos"
+import 'aos/dist/aos.css'
 const Services = () => {
+  useEffect (() =>{
+    AOS.init({});
+  },[])
   return (
     <div>
         <div className="bg-[#198cf6] md:w-[205px] w-[120px] h-[50px] md:h-[68px] text-[#fff] text-[15px] md:text-[25px] font-inter font-semibold flex justify-center items-center">Services</div>
@@ -11,10 +17,11 @@ const Services = () => {
                 <h2 className="text-[#000] font-inter font-medium text-[15px] md:text-[25px] px-4 md:px-[63px]">We provide platforms for individuals to learn and grow in the ever-evolving tech space.</h2>
             </div>
             <div className="flex flex-col md:flex-row justify-between md:mt-[118px] mt-[40px] px-4 md:px-[63px]">
-                  <div>
+                  <div data-aos="fade-right"
+                    data-aos-duration="1000">
                      <img src={service} alt="service" className="mt-10 w-full  px-[20px] md:px-0"/>
                   </div>
-                  <div>
+                  <div  data-aos="fade-left" data-aos-duration="3000">
                     <h2 className="lg:w-[752px] md:w-[500px] font-light text-[#000] md:text-[25px] text-[15px] font-Montserrat"><p className="font-medium  flex justify-center items-center md:justify-start mt-10 md:mt-0">- Boot-Camps</p>
                       <p className="mt-10 flex text-center md:text-start">Explore our specialized boot-camps programs designed 
                       to fast-track your skills and knowledge 
@@ -29,7 +36,7 @@ const Services = () => {
             </div>
             <div className="h-2 lg:w-[700px] w-200px bg-[#198cf6] flex  justify-center items-center lg:mx-80 mx-[50px]  md:mx-[152px]  mt-[105px]"></div>
                 <div className="flex flex-col-reverse md:flex-row justify-between mt-[80px] md:mt-[118px] px-4 md:px-[63px]">  
-                  <div>
+                  <div data-aos="zoom-out-left">
                     <h2 className="lg:w-[752px] md:w-[500px] font-light text-[#000] text-[15px] md:text-[25px] font-Montserrat"><p className="font-medium flex justify-center items-center md:justify-start">- Live Training Sessions</p>
                       <p className="mt-10 flex text-center md:text-start">
                         Join our interactive live training
@@ -40,17 +47,17 @@ const Services = () => {
                            with the best in the field.</p>
                     </h2>
                   </div>
-                  <div className="mt-6 ">
+                  <div className="mt-6 " data-aos="flip-left">
                       <img src={service_1} alt="service" className="md:mt-10 mt:0 mb-10 md:mb-0  md:w-full px-[20px] md:px-0" />
                   </div>
             </div>
             <div className="h-2 lg:w-[700px] w-200px bg-[#198cf6] flex justify-center items-center lg:mx-80 md:mx-[152px] mx-[50px]  mt-[105px]"></div>
 
                 <div className="flex flex-col md:flex-row justify-between md:mt-[118px] mt-[80px] gap-8 px-4 md:px-[63px]">  
-                <div className="banner">
+                <div className="banner" data-aos="zoom-in-down">
                       <img src={service_2} alt="service" className="mt-8 w-full md:w-full px-[20px] md:px-0"/>
                   </div>
-                  <div>
+                  <div data-aos="zoom-in-down">
                     <h2 className="lg:w-[752px] md:w-[500px] font-light text-[#000] text-[15px] md:text-[25px]  font-Montserrat"><p className="font-medium  flex justify-center items-center md:justify-start">- Events</p>
                       <p className="mt-10 flex text-center md:text-start">
                        Stay Informed and Engaged! Explore our upcoming webinars, worskshops, and conferences to keep up with the latest trends in education technology, connect with eperts, and enhance your teaching and learning experience.</p>
