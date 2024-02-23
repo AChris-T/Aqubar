9a697d5529d3982f4b15a21296b0ccf2699ef4e2
chris <taiwoakinfenwa2019@gmail.com>
2024-02-23 20:20:52 +0100

    landingPAge

--- src/Components/EventsComponents/PastEvents.jsx
+++ src/Components/EventsComponents/PastEvents.jsx
@@ -4,7 +4,7 @@ const PastEvents = () => {
     <div className="md:px-[40px] py-[40px] px-4  lg:px-[60px] bg-gray-100">
       <h3 className="font-medium font-Montserrat leading-[48.76px] md:text-[40px] text-[30px] ">Past Events</h3>
       <p className="font-normal md:text-[20px] text-[15px]  font-Montserrat">View Our Slides from previous events</p>
-      <div className="flex flex-col md:flex-row gap-20 px-10 py-8 bg-white md:h-[400px] mt-4">
+      <div className="flex flex-col md:flex-row md:gap-20 gap-10 md:px-10 px-3 md:py-8 py-3 bg-white md:h-[400px] mt-4">
         <img src={pastPic} alt=""/>
         <div>
           <h3 className="font-medium font-Montserrat md:text-[35px] text-[25px] md:leading-[42.67px]">Exploring The Power Of Cloud, Blockchain and Cybersecurity.</h3>
