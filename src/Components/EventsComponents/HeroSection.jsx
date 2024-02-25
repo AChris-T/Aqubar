061324d31686d1a0d56929d7b44248d5ef5327ab
chris <taiwoakinfenwa2019@gmail.com>
2024-02-25 20:58:38 +0100

    Carousel

--- src/Components/EventsComponents/HeroSection.jsx
+++ src/Components/EventsComponents/HeroSection.jsx
@@ -8,11 +8,11 @@ const HeroSection = () => {
             <div className="flex items-center justify-between mb-2 "> 
                 <div className="flex items-center w-full gap-2 md:gap-10 font-Montserrat ">  
                 <div className="w-[40px] rounded-full  border-[1px] border-white h-[40px]"></div>
-                <h3 className="font-medium w-[200px] md:w-[500px] md:leading-[60.95px] leading-8 text-[20px]  md:text-[50px] text-white">Discover Our Upcoming events!</h3>
+                <h3 className="font-medium w-[250px] md:w-[500px] md:leading-[60.95px] leading-8 text-[25px]  md:text-[50px] text-white">Discover Our Upcoming events!</h3>
                </div>
                  <div className="md:w-[40px] w-[45px] rounded-full  border-[1px] border-white h-[40px]"></div>
             </div>
-                <p className="text-white md:ml-[90px] ml-[45px] font-normal text-[20px] md:leading-[24.38px]">Welcome to our Events section!<br/> Get ready for exciting happenings by checking out <br/> our upcoming events.</p>
+                <p className="text-white md:ml-[90px] ml-[45px] font-normal text-[16px] md:leading-[24.38px]">Welcome to our Events section!<br/> Get ready for exciting happenings by checking out <br/> our upcoming events.</p>
             </div>
         </div>
       </div>
--- src/Pages/EventPage/EventPage.jsx
+++ src/Pages/EventPage/EventPage.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react"
 import EventsSlider from "../../Components/EventsComponents/EventsSlider"
 import HeroSection from "../../Components/EventsComponents/HeroSection"
 import PastEvents from "../../Components/EventsComponents/PastEvents"
@@ -6,6 +7,12 @@ import Footer from "../../Components/Modals/Footer"
 import Navbar from "../../Components/Modals/Navbar"
 
 const EventPage = () => {
+  useEffect(()=>{
+    window.scrollTo({
+      top:0,
+      behavior:"smooth"
+    })
+  })
   return (
     <div className="max-w-[1500px]  bg-[#FAFAF9] mx-auto shadow-card overflow-x-hidden">
       <Navbar/>
